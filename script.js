0989b0dc214abbed71db18af593fee7e27705d01
schismatiic <leonardojofre2003@gmail.com>
2025-09-19 13:01:40 -0300

    Rock paper scissors listo. Proyecto finalizado
    Agregué las últimas funciones y ajusté algunos detalles. El programa está terminado.

--- script.js
+++ script.js
@@ -1,49 +1,74 @@
-const getComputerChoice = () => {
-  const option = Math.random();
-  if (option < 0.33) {
-    return "rock";
-  } else if (option >= 0.33 && option < 0.66) {
-    return "paper";
-  } else if (option >= 0.66 && option < 0.99) {
-    return "scissors";
-  }
-};
-const getHumanChoice = () => {
-  const option = prompt(
-    "===== Rock paper scissors ===== \n Type your choice: "
-  );
-  return option;
-};
+const playGame = () => {
+  const getComputerChoice = () => {
+    const option = Math.random();
+    if (option < 0.33) {
+      return "rock";
+    } else if (option >= 0.33 && option < 0.66) {
+      return "paper";
+    } else if (option >= 0.66 && option < 0.99) {
+      return "scissors";
+    }
+  };
+  const getHumanChoice = (r) => {
+    if (r === 0) {
+      alert("You will play 5 rounds");
+    }
+    let option = "";
+    while (option === "") {
+      option = prompt(
+        `===== Rock paper scissors ===== \n Round: ${
+          r + 1
+        } \n Type your choice: `
+      );
+      option = option.toLowerCase();
+      if (option === "rock" || option === "paper" || option === "scissors") {
+        return option;
+      }
+      alert("Try again with a valid option, stupid");
+      option = "";
+    }
+    return option;
+  };
 
-let humanScore = 0;
-let computerScore = 0;
-
-const playRound = (humanChoice, computerChoice) => {
-  humanChoice = humanChoice.toLowerCase();
-  if (
-    humanChoice === "rock" ||
-    humanChoice === "paper" ||
-    humanChoice === "scissors"
-  ) {
+  let humanScore = 0;
+  let computerScore = 0;
+  let round = 0;
+  //   const humanSelection = getHumanChoice();
+  //   const computerSelection = getComputerChoice();
+  const playRound = (humanChoice, computerChoice) => {
     if (humanChoice === computerChoice) {
-      console.log("Draw!");
+      alert(`${humanScore} - ${computerScore} \n Draw!`);
     } else if (
       (humanChoice === "rock" && computerChoice === "scissors") ||
       (humanChoice === "scissors" && computerChoice === "paper") ||
       (humanChoice === "scissors" && computerChoice === "paper")
     ) {
       humanScore++;
-      console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+      alert(
+        `${humanScore} - ${computerScore} \n You win! ${humanChoice} beats ${computerChoice}`
+      );
     } else {
       computerScore++;
-      console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+      alert(
+        `${humanScore} - ${computerScore} \n You lose! ${computerChoice} beats ${humanChoice}`
+      );
     }
-  } else {
+    round++;
+  };
+  playRound(getHumanChoice(round), getComputerChoice());
+  playRound(getHumanChoice(round), getComputerChoice());
+  playRound(getHumanChoice(round), getComputerChoice());
+  playRound(getHumanChoice(round), getComputerChoice());
+  playRound(getHumanChoice(round), getComputerChoice());
+  if (humanScore > computerScore) {
+    alert(`Human wins! ${humanScore} - ${computerScore}`);
+  } else if (humanScore < computerScore) {
     alert(
-      `[ERROR] ${humanChoice} is not valid for an entry! \n Try again with "rock", "paper" or "scissors"`
+      `Computer wins. So, Allied Mastercomputer won! ${humanScore} - ${computerScore}`
     );
+  } else {
+    alert(`Draw! ${humanScore} - ${computerScore}`);
   }
 };
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-console.log(playRound(humanSelection, computerSelection));
+
+console.log(playGame());
