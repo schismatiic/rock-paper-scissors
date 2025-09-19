044a42f42deb87fb31fb3f015a61609b0de9025a
schismatiic <leonardojofre2003@gmail.com>
2025-09-19 10:14:02 -0300

    Función getComputerChoice lista
    getComputerChoice es una función que retorna "aleatoriamente" una de 3 opciones: Rock, paper o scissors

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+const getComputerChoice = () => {
+  const option = Math.random();
+  if (option < 0.33) {
+    return "Rock";
+  } else if (option >= 0.33 && option < 0.66) {
+    return "Paper";
+  } else if (option >= 0.66 && option < 0.99) {
+    return "Scissors";
+  }
+};
+console.log(getComputerChoice());
