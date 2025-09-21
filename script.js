7bd64b7415373b26a9fcd472af53de5acccc8871
schismatiic <leonardojofre2003@gmail.com>
2025-09-21 11:59:38 -0300

    Interfaz y lógica terminada
    No estuve subiendo mis cambios. Pero la interfaz y todas sus respectivas funciones ya están listas

--- script.js
+++ script.js
@@ -9,84 +9,83 @@ const playGame = () => {
       return "scissors";
     }
   };
-  const getHumanChoice = (r) => {
-    if (r === 0) {
-      alert("You will play 5 rounds");
-    }
-    let option = "";
-    while (option === "") {
-      option = prompt(
-        `===== Rock paper scissors ===== \n Round: ${
-          r + 1
-        } \n Type your choice: `
-      );
-      option = option.toLowerCase();
-      if (option === "rock" || option === "paper" || option === "scissors") {
-        return option;
-      }
-      alert("Try again with a valid option, stupid");
-      option = "";
-    }
-    return option;
-  };
 
   let humanScore = 0;
   let computerScore = 0;
   let round = 0;
-  //   const humanSelection = getHumanChoice();
-  //   const computerSelection = getComputerChoice();
   const playRound = (humanChoice, computerChoice) => {
+    if (round === 5) {
+      const finish = document.createElement("h4");
+      if (humanScore > computerScore) {
+        finish.textContent = `Game finished! Human wins!`;
+        currentGame.style.cssText =
+          "border: 1px solid white; width: 250px; margin: 20px 0; padding: 10px; border-radius: 20px; background-color: white; color: rgb(48, 48, 48); font-weight: 700";
+        currentGame.innerHTML = `${finish.textContent}`;
+        playAgain.appendChild(playButton);
+        playButton.addEventListener("click", playGame);
+        return;
+      } else if (humanScore < computerScore) {
+        finish.textContent = `Game finished! Computer wins. So, Allied Mastercomputer won!`;
+        currentGame.style.cssText =
+          "border: 1px solid white; width: 250px; margin: 20px 0; padding: 10px; border-radius: 20px; background-color: white; color: rgb(48, 48, 48); font-weight: 700";
+        currentGame.innerHTML = `${finish.textContent}`;
+        return;
+      } else {
+        finish.textContent = `Game finished! Draw!`;
+        currentGame.style.cssText =
+          "border: 1px solid white; width: 250px; margin: 20px 0; padding: 10px; border-radius: 20px; background-color: white; color: rgb(48, 48, 48); font-weight: 700";
+        currentGame.innerHTML = `${finish.textContent}`;
+        return;
+      }
+    }
+    currentGame.style.cssText =
+      "border: 1px solid white; width: 250px; margin: 20px 0; padding: 10px; border-radius: 20px";
     if (humanChoice === computerChoice) {
-      alert(`${humanScore} - ${computerScore} \n Draw!`);
+      const draw = document.createElement("h4");
+      draw.textContent = `Draw!`;
+      currentGame.innerHTML = `${draw.textContent}`;
     } else if (
       (humanChoice === "rock" && computerChoice === "scissors") ||
       (humanChoice === "scissors" && computerChoice === "paper") ||
-      (humanChoice === "scissors" && computerChoice === "paper")
+      (humanChoice === "paper" && computerChoice === "scissors")
     ) {
       humanScore++;
-      alert(
-        `${humanScore} - ${computerScore} \n You win! ${humanChoice} beats ${computerChoice}`
-      );
+      const win = document.createElement("h4");
+      win.textContent = `You win! ${humanChoice} beats ${computerChoice}`;
+      currentGame.innerHTML = `${win.textContent}`;
     } else {
       computerScore++;
-      alert(
-        `${humanScore} - ${computerScore} \n You lose! ${computerChoice} beats ${humanChoice}`
-      );
+      const lose = document.createElement("h4");
+      lose.textContent = `You lose! ${computerChoice} beats ${humanChoice}`;
+      currentGame.innerHTML = `${lose.textContent}`;
     }
+    const score = document.createElement("p");
+    score.textContent = `${humanScore} -  ${computerScore}`;
+    scoreScreen.style.display = "flex";
+    scoreScreen.innerHTML = `<p> ${score.textContent} </p>`;
     round++;
   };
   buttonContainer.removeChild(playButton);
-
   const options = ["rock", "paper", "scissors"];
-  options.forEach((option) => {
+  for (let index = 0; index < 6; index++) {
     const btn = document.createElement("button");
-    btn.textContent = option;
-    buttonContainer.appendChild(btn);
-    btn.setAttribute("id", option);
-  });
-  playRound(getHumanChoice(round), getComputerChoice());
-
-  // ====================== Score ======================
-  const score = document.createElement("p");
-  score.textContent = `${humanScore} -  ${computerScore}`;
-  buttonContainer.appendChild(score);
-  // =========================================================
-
-  if (humanScore > computerScore) {
-    alert(`Human wins! ${humanScore} - ${computerScore}`);
-  } else if (humanScore < computerScore) {
-    alert(
-      `Computer wins. So, Allied Mastercomputer won! ${humanScore} - ${computerScore}`
+    btn.style.cssText = "margin: 4px; width: 120px;";
+    btn.textContent = options[index].toUpperCase();
+    buttonRps.appendChild(btn);
+    btn.addEventListener("click", () =>
+      playRound(options[index], getComputerChoice())
     );
-  } else {
-    alert(`Draw! ${humanScore} - ${computerScore}`);
   }
 };
-// ====================== Play button ======================
+
 const buttonContainer = document.querySelector(".box__buttons");
+const buttonRps = document.querySelector(".rps__button");
+const scoreScreen = document.querySelector(".score");
+const currentGame = document.querySelector(".current__game");
 const playButton = document.createElement("button");
+const playAgain = document.createElement("div");
 playButton.textContent = "PLAY";
 playButton.style.cssText = "";
 buttonContainer.appendChild(playButton);
-// =========================================================
+
 playButton.addEventListener("click", playGame);
